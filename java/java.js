3b1ae0213868c3246aeb9215351df19ebba4b67e
Matías Herrera <RocioPerdomo>
2023-02-01 20:14:04 -0300

    Se agrega el proyecto

---
+++ java/java.js
@@ -0,0 +1,29 @@
+document.getElementById('nombre').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="Rocio Nicole Perdomo";
+
+});
+
+document.getElementById('email').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="perdomo.rocionicole@gmail.com";
+
+});
+
+document.getElementById('fecha').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="07/07/1999";
+
+});
+
+document.getElementById('dire').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="Castelar 2110";
+
+});
+
+document.getElementById('tel').addEventListener ('mouseover',function(){
+
+document.getElementById('texto').innerHTML="11-22469166";
+
+});
